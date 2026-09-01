e990586cbce9c81378b34bedb07d6e486aba3673
tactik8 <admin@tactik8.com>
2026-09-01 08:55:17 -0400

    go

--- src/server.js
+++ src/server.js
@@ -41,9 +41,7 @@ const storage = multer.diskStorage({
     }
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    const ext = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+    cb(null, file.originalname);
   }
 });
 
